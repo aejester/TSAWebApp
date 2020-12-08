c5a53a5522eeb3a419d48e9b9567a04204c45c50
Belton <beltonbelton123@gmail.com>
2020-12-08 13:21:27 -0500

    update

---
+++ backend/includes/table.php
@@ -0,0 +1,45 @@
+<?php include("connect.php");
+
+$result = $tsaConn->query("select * from events");
+
+while($row=$result->fetch_assoc()){
+    echo "<tr>";
+    echo "<td id=\"eventId\" scope=\"col\">".$row["id"]."</td>";
+    echo "<td scope=\"col\">".$row["eName"]."</td>";
+    echo "<td scope=\"col\">".$row["isTeamEvent"]."</td>";
+    echo "<td scope=\"col\">".$row["max"]."</td>";
+    echo "<td scope=\"col\">".$row["seats"]."</td>";
+    
+    // register if available and not already signed up
+    $reg = false;
+    if(isset($_SESSION["id"])){
+        $res2 = $tsaConn->query("select * from registration where userID=".$_SESSION["id"]);
+    }
+        
+    while($row2 = $res2->fetch_assoc()){
+        if($row2["eventID"] == $row["id"]){
+            echo "<td scope=\"col\"> <button type=\"button\" id=\"team\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#exampleModal\">
+            See Teams
+            </button></td>";
+            echo "<td scope=\"col\"> Enter Preferred Team ID: <input/> <button class=\"btn btn-primary\">Submit</button> </td>";
+            $reg = true;
+        }
+    }
+    
+    if(!$reg && $row["seats"] < $row["max"]){
+        echo "<td scope=\"col\"> <button type=\"button\" id=\"team\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#exampleModal\">
+            See Teams
+            </button></td>";
+        echo "<td scope=\"col\"> 
+        <button id=\"register\" class=\"btn btn-primary\"> Register </button>
+        </td>";
+    }else if(!$reg && $row["seats"] == $row["max"]){
+        echo "<td scope=\"col\"> <button type=\"button\" id=\"team\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#exampleModal\">
+            See Teams
+            </button></td>";
+        echo "<td scope=\"col\"> full </td>";
+    }
+    echo "</tr>";
+}
+
+?>
---
+++ events.php
@@ -0,0 +1,128 @@
+<?php session_start() ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Hello, world!</title>
+    <style>
+        table{width:100%;}
+    </style>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">TSA Web App</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <?php if($_SESSION["first"] == ""){?>
+                <li class="nav-item active">
+                    <a class="nav-link" href="./index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+            <?php }else{ ?>
+                <li id="logout" class="nav-item active">
+                   <button class="btn btn-primary">Logout</button>
+                </li>
+            <?php } ?>
+          <li class="nav-item">
+            <a class="nav-link" href="./events.php">Events</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+    
+    <h1>Events</h1>
+    <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Event ID</th>
+        <th scope="col">Event Name</th>
+        <th scope="col">Individual or Team</th>
+        <th scope="col"># per Team</th>
+        <th scope="col">Seats Available</th>
+        <th scope="col">Teams</th>
+
+        <!--<th scope="col">Regionally Required</th>-->
+        <!--<th scope="col">State Max</th>-->
+        <!--<th scope="col">National Max</th>-->
+        <!--<th scope="col">IsQualifying</th>-->
+        <!--<th scope="col">IsOnsite</th>-->
+        <th scope="col">Register</th>
+      </tr>
+      <?php include("backend/includes/table.php")?>
+    </thead>
+    </table>
+
+    </div>
+   
+   <?php require("backend/includes/footer.php") ?> 
+   <script>
+        
+        $(document).ready(() => {
+            $("#logout").click(() => {
+                $.ajax({
+                    url:"backend/scripts/logout.php",
+                    method:"post",
+                    data:{},
+                    success: (response) => {
+                        location.reload(true);
+                    }
+                }); //end ajax
+            }); //end logout click
+            
+            $("button#register").click(function(){
+                $eventId = $(this).parent().prev().prev().prev().prev().prev().prev().text();
+                $.ajax({
+                    url:"backend/scripts/register.php",
+                    method:"post",
+                    data:{
+                        "eId":$eventId
+                    },
+                    success: (response) => {
+                        location.reload(true);
+                    }
+                }); //end ajax
+            }); //end register click
+            
+            $("button#team").click(function(){
+                $eventId = $(this).parent().prev().prev().prev().prev().prev().text();
+                $.ajax({
+                    url:"backend/scripts/eventInfo.php",
+                    method:"post",
+                    data: {"eID":$eventId},
+                    success: function(response){
+                        let split = response.split("---");
+                        $(".modal-title").html(split[0])
+                        $(".modal-body").html(split[1])
+                    }
+                });//end ajax
+            }); //end team click
+        }); //end document ready
+   </script>
+   
+   <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel" ></h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body"></div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   </body>
+</html>
